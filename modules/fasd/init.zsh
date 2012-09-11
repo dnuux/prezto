16f77f61fbc93d4a0cbc45a887732fa070ba2641
dnuux <dnuuxx@gmail.com>
2012-09-11 16:51:55 +0300

    Updates to fasd module

--- modules/fasd/init.zsh
+++ modules/fasd/init.zsh
@@ -1,9 +1,9 @@
 #
-# Command-line productivity booster, offers quick access to files and directories.
+# Maintains a frequently used file and directory list for fast access.
 #
 # Authors:
-#   Sorin Ionescu <sorin.ionescu@gmail.com>
 #   Wei Dai <x@wei23.net>
+#   Sorin Ionescu <sorin.ionescu@gmail.com>
 #
 
 # Return if requirements are not found.
@@ -11,30 +11,47 @@ if (( ! $+commands[fasd] )); then
   return 1
 fi
 
+#
+# Initialization
+#
+
 cache_file="${0:h}/cache.zsh"
 if [[ "${commands[fasd]}" -nt "$cache_file" || ! -s "$cache_file" ]]; then
-  # Base init arguments
-  init_args='posix-alias zsh-hook'
+  # Set the base init arguments.
+  init_args=(posix-alias zsh-hook)
 
-  # Load zsh-{ccomp,wcomp} when completion is loaded
+  # Set fasd completion init arguments, if applicable.
   if zstyle -t ':prezto:module:completion' loaded; then
-    init_args+=' zsh-ccomp zsh-ccomp-install zsh-wcomp zsh-wcomp-install'
+    init_args+=(zsh-ccomp zsh-ccomp-install zsh-wcomp zsh-wcomp-install)
   fi
 
-  # Cache init code
-  fasd --init "${(z)init_args[@]}" >! "$cache_file" 2> /dev/null
+  # Cache init code.
+  fasd --init "$init_args[@]" >! "$cache_file" 2> /dev/null
 fi
 
 source "$cache_file"
 
 unset cache_file init_args
 
+#
+# Aliases
+#
+
 alias j='z'                       # For autojump converts.
 alias v='f -t -e vim -b viminfo'  # Quickly open files with vim.
 
+#
+# Key Bindings
+#
+
 for keymap in 'emacs' 'viins'; do
-  bindkey -M "$keymap" "$key_info[Control]X$key_info[Control]A" fasd-complete    # C-x C-a (files and directories)
-  bindkey -M "$keymap" "$key_info[Control]X$key_info[Control]D" fasd-complete-d  # C-x C-d (directories)
-  bindkey -M "$keymap" "$key_info[Control]X$key_info[Control]F" fasd-complete-f  # C-x C-f (files)
+  # C-x C-a completes files and direcotries.
+  bindkey -M "$keymap" "$key_info[Control]X$key_info[Control]A" fasd-complete
+
+  # C-x C-d completes directories.
+  bindkey -M "$keymap" "$key_info[Control]X$key_info[Control]D" fasd-complete-d
+
+  # C-x C-f completes files.
+  bindkey -M "$keymap" "$key_info[Control]X$key_info[Control]F" fasd-complete-f
 done
 
