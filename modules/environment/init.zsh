16e1ef395eea03e8ebf95c51e7a0b990b5413d5a
dnuux <dnuuxx@gmail.com>
2012-09-18 14:38:53 +0300

    Unnecessary styling tweaks

--- modules/environment/init.zsh
+++ modules/environment/init.zsh
@@ -16,20 +16,20 @@ zle -N self-insert url-quote-magic
 # General
 #
 
-setopt INTERACTIVECOMMENTS  # Allow comments in commands.
-setopt BRACE_CCL            # Allow brace character class list expansion.
-setopt RC_QUOTES            # Allow 'Henry''s Garage' instead of 'Henry'\''s Garage'.
-unsetopt MAIL_WARNING       # Don't print a warning message if a mail file has been accessed.
+setopt INTERACTIVE_COMMENTS  # Allow comments in commands.
+setopt BRACE_CCL             # Allow brace character class list expansion.
+setopt RC_QUOTES             # Allow 'Henry''s Garage' instead of 'Henry'\''s Garage'.
+unsetopt MAIL_WARNING        # Don't print a warning message if a mail file has been accessed.
 
 #
 # Jobs
 #
 
-setopt LONG_LIST_JOBS       # List jobs in the long format by default.
-setopt AUTO_RESUME          # Attempt to resume existing job before creating a new process.
-setopt NOTIFY               # Report status of background jobs immediately.
-unsetopt HUP                # Don't kill jobs on shell exit.
-unsetopt CHECK_JOBS         # Don't report on jobs when shell exit.
+setopt LONG_LIST_JOBS        # List jobs in the long format by default.
+setopt AUTO_RESUME           # Attempt to resume existing job before creating a new process.
+setopt NOTIFY                # Report status of background jobs immediately.
+unsetopt HUP                 # Don't kill jobs on shell exit.
+unsetopt CHECK_JOBS          # Don't report on jobs when shell exit.
 
 #
 # Grep
